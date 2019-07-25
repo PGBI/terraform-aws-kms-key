cf62954a4eeb9b41c17d17e2dfa266ecc121f5f4
PGBI <pierre.barrau@mines-paris.org>
2019-07-25 11:57:59 -0700

    no markdown in variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "project" {
-  description = "Reference to a [project module](https://registry.terraform.io/modules/PGBI/project/aws/)"
+  description = "Reference to a \"project\" module. See: https://registry.terraform.io/modules/PGBI/project/aws/"
 }
 
 variable "description" {
@@ -19,7 +19,7 @@ variable "iam_consumers_arns" {
 }
 
 variable "consuming_aws_services" {
-  description = "List of [AWS Services](https://gist.github.com/shortjared/4c1e3fe52bdfa47522cfe5b41e5d6f22) that should be allowed to use the key."
+  description = "List of identifiers of AWS Services that should be allowed to use the key. See: https://gist.github.com/shortjared/4c1e3fe52bdfa47522cfe5b41e5d6f22 for a list of AWS Services identifiers."
   type        = list(string)
   default     = []
 }
